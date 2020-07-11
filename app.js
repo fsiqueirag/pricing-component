531e26ce348b6c01ae35478ade5501903900cf8a
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2020-07-10 21:07:00 -0300

    todo

---
+++ app.js
@@ -0,0 +1,25 @@
+let slider = document.querySelector(".slider");
+let pricesLeft = document.getElementById("price-left");
+let pricesMiddle = document.getElementById("price-middle");
+let pricesRight = document.getElementById("price-right");
+let checkboxes = document.getElementById('checkbox');
+let estado = true;
+checkboxes.checked = false;
+slider.addEventListener('click', changePrices);
+
+
+function changePrices() {
+    if (estado === false) {
+        pricesLeft.innerText = "$199.99";
+        pricesMiddle.innerText = "$249.99";
+        pricesRight.innerText = "$399.99";
+        estado = true;
+    } else {
+        pricesLeft.innerText = "$19.99";
+        pricesMiddle.innerText = "$24.99";
+        pricesRight.innerText = "$39.99";
+        estado = false;
+    }
+
+}
+
