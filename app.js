7dcb9d9fe04eddb227d7217767566772b05e198c
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2020-07-10 21:37:16 -0300

    arreglar bug del switch

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-let slider = document.querySelector(".slider");
+let slider = document.getElementById('checkbox');
 let pricesLeft = document.getElementById("price-left");
 let pricesMiddle = document.getElementById("price-middle");
 let pricesRight = document.getElementById("price-right");
 let checkboxes = document.getElementById('checkbox');
-let estado = true;
+let estado = false;
 checkboxes.checked = false;
 slider.addEventListener('click', changePrices);
-pricesLeft.innerText = "$199.99";
-pricesMiddle.innerText = "$249.99";
-pricesRight.innerText = "$399.99";
 
 function changePrices() {
-    if (estado === false) {
+    if (estado === true) {
         pricesLeft.innerText = "$199.99";
         pricesMiddle.innerText = "$249.99";
         pricesRight.innerText = "$399.99";
-        estado = true;
     } else {
         pricesLeft.innerText = "$19.99";
         pricesMiddle.innerText = "$24.99";
         pricesRight.innerText = "$39.99";
-        estado = false;
     }
-
+    estado = !estado;
 }
 
